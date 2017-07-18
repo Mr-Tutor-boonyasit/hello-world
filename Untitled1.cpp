87d14b14dee16353ae982e743bff9f402c9e5997
Mr.Tutor Boonyasit <tutor281242@gmail.com>
2017-07-18 18:38:57 +0700

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	printf("hello world\n");
+	Printf
+	printf("name\n\n\n");
+	printf("hi\n");
+	printf("how are you\n");
+	printf("what's your name\n");
+	getch();
+}
