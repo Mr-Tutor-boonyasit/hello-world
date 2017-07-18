2d599d5c9e4ff0fea412e97541883357e284d7bf
Mr.Tutor Boonyasit <tutor281242@gmail.com>
2017-07-18 18:41:54 +0700

    Add files via upload

---
+++ classroom2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a = 100;
+	int input = 0;
+	cout << "Enter some number that you think is more than 100";
+	cin >> input;
+	cout << endl;
+	cout << "input is :" << input << endl;
+	//if-else
+	if( input > a) {
+	cout << "Yeah!! input more than 100" << endl;
+	}
+	else if( input == 100){
+		cout << "Ok! input is 100" << endl;
+	}
+	else {
+		cout << "oh!! try again!!" << endl;
+	}
+	
+	return 0;
+}
