ba1c23c75de1b729056103406a866def07a6f21e
Mr.Tutor Boonyasit <tutor281242@gmail.com>
2017-07-18 18:40:23 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+/* multilines */
+// oneline
+// data-type name = value
+int a = 10 ;
+int b = 30 ;
+int c = a+b;
+double d = (double)a/b;
+int e = a+b+c+d;
+char i[] = "we" ;
+cout << i << endl;
+//problem a+b
+//problem +,-,*,/
+//problem a-b
+//problem a*b
+//problem a/b
+cout << a+b << endl;
+cout << a-b << endl;
+cout << a*b << endl;
+cout << (float)a/b << endl;
+cout << e*e << endl;
+
+	return 0;
+}
